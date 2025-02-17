e29bf591950b6b63f7cdeb0645e8df376c4ec586
jay-mm-message <jay.mm.message@gmail.com>
2025-02-17 13:53:15 +0800

    Typesetting

--- src/strtype3.cpp
+++ src/strtype3.cpp
@@ -3,24 +3,25 @@
 #include <string>
 #include <cstring>
 
-int main(){
+int main()
+{
   using namespace std;
   char charr1[20];
-  char charr2[20]="jaguar";
+  char charr2[20] = "jaguar";
   string str1;
-  string str2="panther";
+  string str2 = "panther";
 
-  str1=str2;
-  strcpy(charr1,charr2);
+  str1 = str2;
+  strcpy(charr1, charr2);
 
-  str1+=" paste";
-  strcat(charr1," juice");
+  str1 += " paste";
+  strcat(charr1, " juice");
 
-  int len1=str1.size();
-  int len2=strlen(charr1);
+  int len1 = str1.size();
+  int len2 = strlen(charr1);
 
-  cout<<"The string "<<str1<<" contains "<<len1<<" characters.\n";
-  cout<<"The string "<<charr1<<" contains "<<len2<<" characters.\n";
+  cout << "The string " << str1 << " contains " << len1 << " characters.\n";
+  cout << "The string " << charr1 << " contains " << len2 << " characters.\n";
 
   //  char* str3="Hello world";
   //  strcpy(str3,"error");
