9b9e66293bdc4a18e31d4e7f387a374424fe9128
jay-mm-message <jay.mm.message@gmail.com>
2025-02-17 11:24:08 +0800

    Typesetting

--- src/numstr.cpp
+++ src/numstr.cpp
@@ -1,16 +1,17 @@
 // numstr.cpp -- following number input with line input
 #include <iostream>
-int main(){
+int main()
+{
   using namespace std;
 
-  const int ArSize=20;
+  const int ArSize = 20;
   char name[ArSize];
   long money;
-  cout<<"Enter your name:\n";
-  cin>>name;
-  cout<<"Enter your money:\n";
-  cin>>money;
-  cout<<"You "<<name<<" have "<<money<<" dollars.\n";
+  cout << "Enter your name:\n";
+  cin >> name;
+  cout << "Enter your money:\n";
+  cin >> money;
+  cout << "You " << name << " have " << money << " dollars.\n";
 
   return 0;
 }
