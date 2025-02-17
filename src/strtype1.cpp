8116971d1ba790f41a76e1dff0b201e4297537ff
jay-mm-message <jay.mm.message@gmail.com>
2025-02-17 13:47:05 +0800

    refactor

--- src/strtype1.cpp
+++ src/strtype1.cpp
@@ -5,20 +5,26 @@ int main()
 {
   using namespace std;
 
-  char charr1[20];
-  char charr2[20] = "jaguar";
-  string str1;
+  const int sz = 20;
+  //char charr1[20];
+  char charr2[sz] = "jaguar";
+  //string str1;
   string str2 = "panther";
 
   cout << "Enter a kind of feline: ";
-  cin >> charr1;
+  // (cin >> charr1).get(); //ocelot 
+  char charr1[sz] {"ocelot"};
   cout << "Enter another kind of feline: ";
-  cin >> str1;
+  // (cin >> str1).get();  //tiger
+  string str1 {"tiger"};
+  
   cout << "Here are some felines:\n";
   cout << charr1 << " " << charr2 << " "
        << str1 << " " << str2 << endl;
+  cout << "The third letter in " << charr2 << " is "
+       << charr2[2] << endl;
   cout << "The third letter in " << str2 << " is "
-       << str2[2] << endl;
+       << str2.at(2) << endl;
 
   return 0;
 }
