d9b449eb1ef45065fceabe508ef1782ee58ca684
jay-mm-message <jay.mm.message@gmail.com>
2025-02-18 11:11:19 +0800

    Typesetting

--- src/arrstruc.cpp
+++ src/arrstruc.cpp
@@ -1,20 +1,21 @@
 // arrstruc.cpp -- an array of structures
 #include <iostream>
-struct inflatable{
+struct inflatable
+{
   char name[20];
   float volume;
   double price;
 };
-int main(){
+int main()
+{
   using namespace std;
-  inflatable guest[2]={
-    {"Bambi",0.5,21.99},
-    {"Godzilla",2000,565.99}
-  };
+  inflatable guest[2] = {ß
+      {"Bambi", 0.5, 21.99},
+      {"Godzilla", 2000, 565.99}};
 
-  cout<<"The guests "<<guest[0].name<<" and "
-      <<guest[1].name<<"\nhave a combined volum of "
-      <<guest[0].volume+guest[1].volume<<" cubic feet.\n";
+  cout << "The guests " << guest[0].name << " and "
+       << guest[1].name << "\nhave a combined volum of "
+       << guest[0].volume + guest[1].volume << " cubic feet.\n";
 
   return 0;
 }
