336075e5da0cb45ac98a5436c8c73ffef6b1c3dc
jay-mm-message <jay.mm.message@gmail.com>
2025-02-18 11:30:32 +0800

    Add union test case

--- src/arrstruc.cpp
+++ src/arrstruc.cpp
@@ -6,6 +6,18 @@ struct inflatable
   float volume;
   double price;
 };
+
+struct widget
+{
+  char name[20];
+  int type;
+  union id
+  {
+    int id_num;
+    char id_char[20];
+  } id_val;
+};
+
 int main()
 {
   using namespace std;
@@ -16,6 +28,38 @@ int main()
   cout << "The guests " << guest[0].name << " and "
        << guest[1].name << "\nhave a combined volume of "
        << guest[0].volume + guest[1].volume << " cubic feet.\n";
+  
+  widget w = {"Widget", 1};
+  cout << "Enter widget type: ";
+  cin >> w.type;
+  cin.get();
+  cout << "Enter id value: ";
+  if (w.type == 1)
+  {
+    cin >> w.id_val.id_num;
+    cout << "id_num: " << w.id_val.id_num << endl;
+  }
+  else
+  {
+    cin.getline(w.id_val.id_char, 20);
+    cout << "id_char: " << w.id_val.id_char << endl;
+  }
+
+  cout << endl;
+  cout << "Enter widget type: ";
+  cin >> w.type;
+  cin.get();
+  cout << "Enter id value: ";
+  if (w.type == 1)
+  {
+    cin >> w.id_val.id_num;
+    cout << "id_num: " << w.id_val.id_num << endl;
+  }
+  else
+  {
+    cin.getline(w.id_val.id_char, 20);
+    cout << "id_char: " << w.id_val.id_char << endl;
+  }
 
   return 0;
 }
