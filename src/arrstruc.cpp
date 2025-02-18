0390f116030fb566f47fe8ed8a68b181bbbbf73f
jay-mm-message <jay.mm.message@gmail.com>
2025-02-18 12:32:03 +0800

    Add embedded union test case

--- src/arrstruc.cpp
+++ src/arrstruc.cpp
@@ -18,6 +18,14 @@ struct widget
   } id_val;
 };
 
+union Register16 {
+  uint16_t value; // 16 位的完整寄存器值
+  struct {
+      uint8_t low;  // 低 8 位
+      uint8_t high; // 高 8 位
+  } bytes;
+};
+
 int main()
 {
   using namespace std;
@@ -29,37 +37,55 @@ int main()
        << guest[1].name << "\nhave a combined volume of "
        << guest[0].volume + guest[1].volume << " cubic feet.\n";
   
-  widget w = {"Widget", 1};
-  cout << "Enter widget type: ";
-  cin >> w.type;
-  cin.get();
-  cout << "Enter id value: ";
-  if (w.type == 1)
-  {
-    cin >> w.id_val.id_num;
-    cout << "id_num: " << w.id_val.id_num << endl;
-  }
-  else
-  {
-    cin.getline(w.id_val.id_char, 20);
-    cout << "id_char: " << w.id_val.id_char << endl;
-  }
+  // widget w = {"Widget", 1};
+  // cout << "Enter widget type: ";
+  // cin >> w.type;
+  // cin.get();
+  // cout << "Enter id value: ";
+  // if (w.type == 1)
+  // {
+  //   cin >> w.id_val.id_num;
+  //   cout << "id_num: " << w.id_val.id_num << endl;
+  // }
+  // else
+  // {
+  //   cin.getline(w.id_val.id_char, 20);
+  //   cout << "id_char: " << w.id_val.id_char << endl;
+  // }
 
-  cout << endl;
-  cout << "Enter widget type: ";
-  cin >> w.type;
-  cin.get();
-  cout << "Enter id value: ";
-  if (w.type == 1)
-  {
-    cin >> w.id_val.id_num;
-    cout << "id_num: " << w.id_val.id_num << endl;
-  }
-  else
-  {
-    cin.getline(w.id_val.id_char, 20);
-    cout << "id_char: " << w.id_val.id_char << endl;
-  }
+  // cout << endl;
+  // cout << "Enter widget type: ";
+  // cin >> w.type;
+  // cin.get();
+  // cout << "Enter id value: ";
+  // if (w.type == 1)
+  // {
+  //   cin >> w.id_val.id_num;
+  //   cout << "id_num: " << w.id_val.id_num << endl;
+  // }
+  // else
+  // {
+  //   cin.getline(w.id_val.id_char, 20);
+  //   cout << "id_char: " << w.id_val.id_char << endl;
+  // }
+
+  Register16 reg;
+  reg.value = 0x1234;
+
+  std::cout << "Full Register Value: 0x" 
+            << std::hex << reg.value << std::endl;
+  std::cout << "High Byte: 0x" 
+            << std::hex << static_cast<int>(reg.bytes.high) << std::endl;
+  std::cout << "Low Byte: 0x" 
+            << std::hex << static_cast<int>(reg.bytes.low) << std::endl;
+
+  reg.bytes.low = 0xAB;
+  std::cout << "Full Register Value: 0x" 
+            << std::hex << reg.value << std::endl;
+  std::cout << "High Byte: 0x" 
+            << std::hex << static_cast<int>(reg.bytes.high) << std::endl;
+  std::cout << "Low Byte: 0x" 
+            << std::hex << static_cast<int>(reg.bytes.low) << std::endl;
 
   return 0;
 }
