44b0134aa40d752c33d053e6947a919961898560
jay-mm-message <jay.mm.message@gmail.com>
2025-02-23 15:27:21 +0800

    Change value into wages

--- src/addpntrs.cpp
+++ src/addpntrs.cpp
@@ -3,7 +3,7 @@
 int main()
 {
   using namespace std;
-  double wages[3] = {100, 200, 300};
+  double wages[3] = {10000.0, 20000.0, 30000.0};
   short stacks[3] = {3, 2, 1};
 
   double *pw = wages;
