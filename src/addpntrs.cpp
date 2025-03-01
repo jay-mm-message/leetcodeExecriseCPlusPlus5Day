40f069bfe45f377e207ce5de246bc651aec95264
jay-mm-message <jay.mm.message@gmail.com>
2025-03-02 02:08:34 +0800

    refactor

--- src/addpntrs.cpp
+++ src/addpntrs.cpp
@@ -29,5 +29,32 @@ int main()
 
   cout << "sizeof(wages) = " << sizeof(wages) << endl;
   cout << "sizeof(pw) = " << sizeof(pw) << endl;
+
+  short tell[10];
+  cout << tell << endl;
+  cout << &tell << endl;
+  cout << &tell[0] << endl;
+
+  for(int it = 0 ; it < 10 ; it++)
+  {
+    tell[it] = it + 1;
+    cout << tell[it] << ' ';
+  }
+  cout << endl;
+
+  short (*pt)[10] = &tell;
+  for(int ipt = 0 ; ipt < 10 ; ipt++)
+  { 
+    (*pt)[ipt] = (*pt)[ipt] + 1;
+  }
+
+  for(int it = 0 ; it < 10 ; it++)
+  {
+    cout << tell[it] << ' ';
+  }
+  cout << endl;
+  cout << &tell[2] << endl;
+  cout << (*pt)[2] << endl;
+  cout << &(*pt)[2] << endl;
   return 0;
 }
