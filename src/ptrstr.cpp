68cfa30c888bcecd280e63a9257bf13faa128e04
jay-mm-message <jay.mm.message@gmail.com>
2025-03-02 02:20:37 +0800

    Typesetting

--- src/ptrstr.cpp
+++ src/ptrstr.cpp
@@ -1,29 +1,30 @@
 // ptrstr.cpp -- using pointers to strings
 #include <iostream>
 #include <cstring>
-int main(){
+int main()
+{
   using namespace std;
-  char animal[20]="bear";
-  const char *bird="wren";
+  char animal[20] = "bear";
+  const char *bird = "wren";
   char *ps;
 
-  cout<<animal<<" adn "<<bird<<"\n";
+  cout << animal << " adn " << bird << "\n";
 
-  cout<<"Enter a kind of animal: ";
-  cin>>animal;
+  cout << "Enter a kind of animal: ";
+  cin >> animal;
 
-  ps=animal;
-  cout<<ps<<"!\n";
-  cout<<"Before using strcpy():\n";
-  cout<<animal<<" at "<<(int*)animal<<endl;
-  cout<<ps<<" at "<<(int*)ps<<endl;
+  ps = animal;
+  cout << ps << "!\n";
+  cout << "Before using strcpy():\n";
+  cout << animal << " at " << (int *)animal << endl;
+  cout << ps << " at " << (int *)ps << endl;
 
-  ps=new char[strlen(animal)+1];
-  strcpy(ps,animal);
+  ps = new char[strlen(animal) + 1];
+  strcpy(ps, animal);
 
-  cout<<"After using strcpy();\n";
-  cout<<animal<<" at "<<(int*)animal<<endl;
-  cout<<ps<<" at "<<(int*)ps<<endl;
+  cout << "After using strcpy();\n";
+  cout << animal << " at " << (int *)animal << endl;
+  cout << ps << " at " << (int *)ps << endl;
   delete[] ps;
   return 0;
 }
