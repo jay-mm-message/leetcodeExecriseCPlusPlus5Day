6a754073279357413592ddff740c727810f7a5a8
jay-mm-message <jay.mm.message@gmail.com>
2025-04-29 08:40:31 +0800

    indent

--- src/ourfunc.cpp
+++ src/ourfunc.cpp
@@ -1,21 +1,23 @@
 // ourfunc.cpp -- defining your own function
-#include<iostream>
-void simon(int);		// funciont prototype for simon()
+#include <iostream>
+void simon(int); // funciont prototype for simon()
 
-int main(){
+int main()
+{
   using namespace std;
   simon(3);
-  cout<<"Pick an interger: ";
+  cout << "Pick an interger: ";
   int count;
-  cin>>count;
+  cin >> count;
   simon(count);
-  cout<<"Done!"<<endl;
+  cout << "Done!" << endl;
 
   return 0;
 }
 
-void simon(int n){
+void simon(int n)
+{
 
   using namespace std;
-  cout<<"Simon says touch your toes "<<n<<" times"<<endl;
+  cout << "Simon says touch your toes " << n << " times" << endl;
 }
