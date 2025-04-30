99e3cc6e1c847902fc881a517ae0fb7c90bfe8e1
jay-mm-message <jay.mm.message@gmail.com>
2025-05-01 07:20:08 +0800

    indent

--- src/hexoct1.cpp
+++ src/hexoct1.cpp
@@ -1,15 +1,16 @@
 // hexoct1.cpp -- shows hex and ctal literals
 
-#include<iostream>
-int main(){
+#include <iostream>
+int main()
+{
   using namespace std;
-  int chest=42;
-  int waist=0x42;
-  int inseam=042;
+  int chest = 42;
+  int waist = 0x42;
+  int inseam = 042;
 
-  cout<<"chest= "<<chest<<" (42 in decimal)"<<endl;
-  cout<<"waist= "<<waist<<" (0x42 in hex)"<<endl;
-  cout<<"inseam= "<<inseam<<" (042 in octal)"<<endl;
+  cout << "chest= " << chest << " (42 in decimal)" << endl;
+  cout << "waist= " << waist << " (0x42 in hex)" << endl;
+  cout << "inseam= " << inseam << " (042 in octal)" << endl;
 
   return 0;
 }
