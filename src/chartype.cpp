3251ee8077af0511c3a411f41e4099191c4a28b9
jay-mm-message <jay.mm.message@gmail.com>
2025-05-01 07:32:54 +0800

    indent

--- src/chartype.cpp
+++ src/chartype.cpp
@@ -1,13 +1,14 @@
 // chartype.cpp -- the char type
 
 #include <iostream>
-int main(){
+int main()
+{
   using namespace std;
 
   char ch;
-  cout<<"Enter a character: "<<endl;
-  cin>>ch;
-  cout<<"Thank you for the "<<ch<<" character"<<endl;
+  cout << "Enter a character: " << endl;
+  cin >> ch;
+  cout << "Thank you for the " << ch << " character" << endl;
 
   return 0;
 }
