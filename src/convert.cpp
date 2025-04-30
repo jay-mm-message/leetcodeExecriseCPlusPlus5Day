15b2546c7212ff38f6a3f93b7d43756d27803e70
jay-mm-message <jay.mm.message@gmail.com>
2025-04-30 08:32:50 +0800

    indent

--- src/convert.cpp
+++ src/convert.cpp
@@ -1,20 +1,22 @@
 // convert.cpp -- convet stone to pounds
 
-#include<iostream>
+#include <iostream>
 
 int stone2lb(int);
-int main(){
+int main()
+{
   using namespace std;
-  
+
   int stone;
-  cout<<"Enter the weight in stones: ";
-  cin>>stone;
-  int pounds=stone2lb(stone);
-  cout<<stone<<" stone = ";
-  cout<<pounds<<" pounes."<<endl;
+  cout << "Enter the weight in stones: ";
+  cin >> stone;
+  int pounds = stone2lb(stone);
+  cout << stone << " stone = ";
+  cout << pounds << " pounes." << endl;
   return 0;
 }
 
-int stone2lb(int sts){
-  return 14*sts;
+int stone2lb(int sts)
+{
+  return 14 * sts;
 }
