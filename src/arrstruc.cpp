36d3bcda53bbd631abc555eb52a63e0d07d29603
jay-mm-message <jay.mm.message@gmail.com>
2025-05-02 05:59:16 +0800

    add arrstruct2.cpp and add arrstruct.cpp

--- src/arrstruc.cpp
+++ src/arrstruc.cpp
@@ -1,155 +1,27 @@
-// arrstruc.cpp -- an array of structures
 #include <iostream>
 struct inflatable
 {
+	/* data */
 	char name[20];
 	float volume;
 	double price;
 };
 
-struct widget
-{
-	char name[20];
-	int type;
-	union id
-	{
-		int id_num;
-		char id_char[20];
-	} id_val;
-};
-
-union Register16
-{
-	uint16_t value; // 16 位的完整寄存器值
-	struct
-	{
-		uint8_t low;  // 低 8 位
-		uint8_t high; // 高 8 位
-	} bytes;
-};
-
 int main()
 {
 	using namespace std;
-	inflatable guest[2] = {
-		{"Bambi", 0.5, 21.99},
-		{"Godzilla", 2000, 565.99}};
-
-	cout << "The guests " << guest[0].name << " and "
-		 << guest[1].name << "\nhave a combined volume of "
-		 << guest[0].volume + guest[1].volume << " cubic feet.\n";
-
-	// widget w = {"Widget", 1};
-	// cout << "Enter widget type: ";
-	// cin >> w.type;
-	// cin.get();
-	// cout << "Enter id value: ";
-	// if (w.type == 1)
-	// {
-	//   cin >> w.id_val.id_num;
-	//   cout << "id_num: " << w.id_val.id_num << endl;
-	// }
-	// else
-	// {
-	//   cin.getline(w.id_val.id_char, 20);
-	//   cout << "id_char: " << w.id_val.id_char << endl;
-	// }
-
-	// cout << endl;
-	// cout << "Enter widget type: ";
-	// cin >> w.type;
-	// cin.get();
-	// cout << "Enter id value: ";
-	// if (w.type == 1)
-	// {
-	//   cin >> w.id_val.id_num;
-	//   cout << "id_num: " << w.id_val.id_num << endl;
-	// }
-	// else
-	// {
-	//   cin.getline(w.id_val.id_char, 20);
-	//   cout << "id_char: " << w.id_val.id_char << endl;
-	// }
-
-	Register16 reg;
-	reg.value = 0x1234;
-
-	std::cout << "Full Register Value: 0x"
-			  << std::hex << reg.value << std::endl;
-	std::cout << "High Byte: 0x"
-			  << std::hex << static_cast<int>(reg.bytes.high) << std::endl;
-	std::cout << "Low Byte: 0x"
-			  << std::hex << static_cast<int>(reg.bytes.low) << std::endl;
-
-	reg.bytes.low = 0xAB;
-	std::cout << "Full Register Value: 0x"
-			  << std::hex << reg.value << std::endl;
-	std::cout << "High Byte: 0x"
-			  << std::hex << static_cast<int>(reg.bytes.high) << std::endl;
-	std::cout << "Low Byte: 0x"
-			  << std::hex << static_cast<int>(reg.bytes.low) << std::endl;
-
-	enum week
+	inflatable guest[2] =
 	{
-		Sunday,
-		Monday,
-		Tuesday,
-		Wednesday,
-		Thursday,
-		Friday,
-		Saturday
+		{"\"Bambi\"", 0.5, 21.99},
+		{"\"Godzilla\"", 2000, 565.99}
 	};
-
-	cout << endl;
-	for (week day = Sunday; day <= Saturday; day = static_cast<week>(day + 1))
-	{
-		cout << "Day is: ";
-		switch (day)
-		{
-		case week::Sunday:
-			/* code */
-			cout << "Sunday" << endl;
-			break;
-		case week::Monday:
-			/* code */
-			cout << "Monday" << endl;
-			break;
-		case week::Tuesday:
-			/* code */
-			cout << "Tuesday" << endl;
-			break;
-		case week::Wednesday:
-			/* code */
-			cout << "Wednesday" << endl;
-			break;
-		case week::Thursday:
-			/* code */
-			cout << "Thursday" << endl;
-			break;
-		case week::Friday:
-			/* code */
-			cout << "Friday" << endl;
-			break;
-		case week::Saturday:
-			/* code */
-			cout << "Saturday" << endl;
-			break;
-		default:
-			break;
-		}
-	}
-
-	week today = week::Tuesday;
-	today = static_cast<week>(today + 1);
-
-	if (today == week::Wednesday)
-	{
-		cout << "Today is Wednesday" << endl;
-	}
-	else
-	{
-		cout << "Today is not Wednesday" << endl;
-	}
-
+	cout << "The inflatable structure " << guest[0].name;
+	cout << " has a volume of " << guest[0].volume;
+	cout << " has a price of $" << guest[0].price;
+	cout << " cubic feet.\n";
+	cout << "The inflatable structure " << guest[1].name;
+	cout << " has a volume of " << guest[1].volume;
+	cout << " has a price of $" << guest[1].price;
+	cout << " cubic feet.\n";
 	return 0;
 }
---
+++ src/arrstruc2.cpp
@@ -0,0 +1,155 @@
+// arrstruc.cpp -- an array of structures
+#include <iostream>
+struct inflatable
+{
+	char name[20];
+	float volume;
+	double price;
+};
+
+struct widget
+{
+	char name[20];
+	int type;
+	union id
+	{
+		int id_num;
+		char id_char[20];
+	} id_val;
+};
+
+union Register16
+{
+	uint16_t value; // 16 位的完整寄存器值
+	struct
+	{
+		uint8_t low;  // 低 8 位
+		uint8_t high; // 高 8 位
+	} bytes;
+};
+
+int main()
+{
+	using namespace std;
+	inflatable guest[2] = {
+		{"Bambi", 0.5, 21.99},
+		{"Godzilla", 2000, 565.99}};
+
+	cout << "The guests " << guest[0].name << " and "
+		 << guest[1].name << "\nhave a combined volume of "
+		 << guest[0].volume + guest[1].volume << " cubic feet.\n";
+
+	// widget w = {"Widget", 1};
+	// cout << "Enter widget type: ";
+	// cin >> w.type;
+	// cin.get();
+	// cout << "Enter id value: ";
+	// if (w.type == 1)
+	// {
+	//   cin >> w.id_val.id_num;
+	//   cout << "id_num: " << w.id_val.id_num << endl;
+	// }
+	// else
+	// {
+	//   cin.getline(w.id_val.id_char, 20);
+	//   cout << "id_char: " << w.id_val.id_char << endl;
+	// }
+
+	// cout << endl;
+	// cout << "Enter widget type: ";
+	// cin >> w.type;
+	// cin.get();
+	// cout << "Enter id value: ";
+	// if (w.type == 1)
+	// {
+	//   cin >> w.id_val.id_num;
+	//   cout << "id_num: " << w.id_val.id_num << endl;
+	// }
+	// else
+	// {
+	//   cin.getline(w.id_val.id_char, 20);
+	//   cout << "id_char: " << w.id_val.id_char << endl;
+	// }
+
+	Register16 reg;
+	reg.value = 0x1234;
+
+	std::cout << "Full Register Value: 0x"
+			  << std::hex << reg.value << std::endl;
+	std::cout << "High Byte: 0x"
+			  << std::hex << static_cast<int>(reg.bytes.high) << std::endl;
+	std::cout << "Low Byte: 0x"
+			  << std::hex << static_cast<int>(reg.bytes.low) << std::endl;
+
+	reg.bytes.low = 0xAB;
+	std::cout << "Full Register Value: 0x"
+			  << std::hex << reg.value << std::endl;
+	std::cout << "High Byte: 0x"
+			  << std::hex << static_cast<int>(reg.bytes.high) << std::endl;
+	std::cout << "Low Byte: 0x"
+			  << std::hex << static_cast<int>(reg.bytes.low) << std::endl;
+
+	enum week
+	{
+		Sunday,
+		Monday,
+		Tuesday,
+		Wednesday,
+		Thursday,
+		Friday,
+		Saturday
+	};
+
+	cout << endl;
+	for (week day = Sunday; day <= Saturday; day = static_cast<week>(day + 1))
+	{
+		cout << "Day is: ";
+		switch (day)
+		{
+		case week::Sunday:
+			/* code */
+			cout << "Sunday" << endl;
+			break;
+		case week::Monday:
+			/* code */
+			cout << "Monday" << endl;
+			break;
+		case week::Tuesday:
+			/* code */
+			cout << "Tuesday" << endl;
+			break;
+		case week::Wednesday:
+			/* code */
+			cout << "Wednesday" << endl;
+			break;
+		case week::Thursday:
+			/* code */
+			cout << "Thursday" << endl;
+			break;
+		case week::Friday:
+			/* code */
+			cout << "Friday" << endl;
+			break;
+		case week::Saturday:
+			/* code */
+			cout << "Saturday" << endl;
+			break;
+		default:
+			break;
+		}
+	}
+
+	week today = week::Tuesday;
+	today = static_cast<week>(today + 1);
+
+	if (today == week::Wednesday)
+	{
+		cout << "Today is Wednesday" << endl;
+	}
+	else
+	{
+		cout << "Today is not Wednesday" << endl;
+	}
+
+	return 0;
+}
