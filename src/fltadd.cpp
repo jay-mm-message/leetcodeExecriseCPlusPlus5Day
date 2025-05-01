0e48081badba4aa9c7af9ea54e108b15eacd8fbb
jay-mm-message <jay.mm.message@gmail.com>
2025-05-01 10:22:32 +0800

    new fltadd.cpp

---
+++ src/fltadd.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+    float a = 2.32E+3f;
+    float b = a - 1.0f;
+
+    using namespace std;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "b - a = " << b - a << endl;
+    return 0;
+}
