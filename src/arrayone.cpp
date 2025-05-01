a990b43b6e8db7947451554adc07e7d267780a70
jay-mm-message <jay.mm.message@gmail.com>
2025-05-01 12:24:32 +0800

    indent

--- src/arrayone.cpp
+++ src/arrayone.cpp
@@ -13,7 +13,7 @@ int main()
      int yam_costs[3] = {20, 30, 5};
      cout << "The total size of the array yam_cost: " << sizeof(yam_costs) << " bytes.\n";
      cout << "The element size of the yam_costs[0]: " << sizeof(yam_costs[0]) << " bytes.\n";
-     
+
      cout << "Total yams= ";
      cout << yams[0] + yams[1] + yams[2] << endl;
      cout << "The package with " << yams[1] << " yams costs "
@@ -30,29 +30,28 @@ int main()
           << " bytes.\n";
 
      int exercise[3] = {1};
-     for(int elem = 0 ; elem < sizeof(exercise)/sizeof(exercise[0]); elem++)
+     for (int elem = 0; elem < sizeof(exercise) / sizeof(exercise[0]); elem++)
      {
           cout << "exercise[" << elem << "] = " << exercise[elem] << endl;
      }
      int idx = 0;
-     for(int e : exercise)
+     for (int e : exercise)
      {
           cout << "exercise[" << idx << "] = " << e << endl;
           ++idx;
      }
 
-     int exercise2[3] {1, 2, 3};
-     for(int elem = 0 ; elem < sizeof(exercise2)/sizeof(exercise2[0]); elem++)
+     int exercise2[3]{1, 2, 3};
+     for (int elem = 0; elem < sizeof(exercise2) / sizeof(exercise2[0]); elem++)
      {
           cout << "exercise2[" << elem << "] = " << exercise2[elem] << endl;
      }
 
-
-     char dogT[2] = {'a','\0'};
+     char dogT[2] = {'a', '\0'};
      cout << "Test null character: " << dogT << endl;
      char dog[8] = {'b', 'e', 'a', 'u', 'x', ' ', 'I', 'I'};
      cout << "The string of dog is: " << dog << endl;
-     //char cat[8] = {'f', 'a', 't', 'e', 's', 's', 'a', '\0'};
+     // char cat[8] = {'f', 'a', 't', 'e', 's', 's', 'a', '\0'};
      char cat[8] = {'f', '\0', 't', 'e', '\0', 's', 'a', '\0'};
      cout << "The string of cat is: " << cat << ", length: " << strlen(cat) << endl;
      return 0;
