0405d8cac3ba57e1265ef805690f8275319e1a8c
jay-mm-message <jay.mm.message@gmail.com>
2025-05-01 08:21:22 +0800

    new bondini

---
+++ src/bondini.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+    using namespace std;
+    cout << "\tOperation \"HyperHyper\" is not activated!\n";
+    cout << "Enter your agent code:____\b\b\b\b";
+    long code;
+    cin >> code;
+    cout << "\tYou entered " << code <<  "...\n";
+    cout << "\tCode verified! Proceed with Plan t shift 3!\n";
+    return 0;
+}
