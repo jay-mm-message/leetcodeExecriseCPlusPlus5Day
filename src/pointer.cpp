36f6f4308d989962ec8c269884602969ee1b910b
jay-mm-message <jay.mm.message@gmail.com>
2025-05-03 07:37:13 +0800

    indent

--- src/pointer.cpp
+++ src/pointer.cpp
@@ -2,20 +2,21 @@
 #include <iostream>
 int main()
 {
-  using namespace std;
-  int updates = 6;
-  int *p_updates;
+	system("clear");
+	using namespace std;
+	int updates = 6;
+	int *p_updates;
 
-  p_updates = &updates;
+	p_updates = &updates;
 
-  cout << "Values: updates= " << updates;
-  cout << ", *p_updates= " << *p_updates << endl;
+	cout << "Values: updates= " << updates;
+	cout << ", *p_updates= " << *p_updates << endl;
 
-  cout << "Addresses: &updates = " << &updates
-       << ", p_updates = " << p_updates << endl;
+	cout << "Addresses: &updates = " << &updates
+		 << ", p_updates = " << p_updates << endl;
 
-  *p_updates = *p_updates + 1;
-  cout << "Now updates = " << updates << endl;
+	*p_updates = *p_updates + 1;
+	cout << "Now updates = " << updates << endl;
 
-  return 0;
+	return 0;
 }
